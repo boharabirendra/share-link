1fce31dec9f6179967356d88791235077fe44b1a
boharabirendra <boharabirendra546@gmail.com>
2024-11-10 08:19:28 +0545

    Added about pages with open graph meta data

---
+++ src/app/about/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: "Trusted technology partner | Leapfrog Technology",
+    description:
+      "Unlocking Innovation and Technology: 450+ experts crafting your ideas into user-friendly products. Drive your growth with us.",
+    images: [
+      {
+        url: "/share_image.jpg",
+        alt: "Alternate text",
+      },
+    ],
+  },
+};
+
+const About = () => {
+  return (
+    <div className="text-center p-4 rounded shadow-sm bg-slate-900">
+      <h1>About page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+        quaerat quasi aut beatae nobis nulla, repellat amet dolore, asperiores
+        perferendis corporis. Nisi, esse alias. Nam inventore ullam quasi
+        quisquam maxime?
+      </p>
+    </div>
+  );
+};
+
+export default About;
