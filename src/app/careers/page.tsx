81a64efaba5fae21da7edc82c0081034c96877c1
boharabirendra <boharabirendra546@gmail.com>
2024-11-12 16:51:07 +0545

    Change careers share preview image

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
       "Embark on a journey that goes beyond just a job. At Leapfrog, you will get an environment that fosters growth, learning, and collaboration. A place which values diverse perspectives, accountable freedom, pushes you to reach your new height, and celebrates both small and big wins.",
     images: [
       {
-        url: "/careers-banner-3x.webp",
+        url: "/share_image.jpg",
         alt: "Alternate text",
       },
     ],
