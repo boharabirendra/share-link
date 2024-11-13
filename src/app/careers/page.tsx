3c159294d714357c60ac989331b3765c9a5de2ab
boharabirendra <boharabirendra546@gmail.com>
2024-11-13 12:13:17 +0545

    Change preview image

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 export const metadata: Metadata = {
   openGraph: {
     title: "Growth mindset. Curious. Innovative. Sounds like you?",
+    url: "https://share-link-six.vercel.app/careers",
     description:
       "Embark on a journey that goes beyond just a job. At Leapfrog, you will get an environment that fosters growth, learning, and collaboration. A place which values diverse perspectives, accountable freedom, pushes you to reach your new height, and celebrates both small and big wins.",
     images: [
       {
-        url: "/share_image.jpg",
+        url: "/share-about.jpg",
         alt: "Alternate text",
       },
     ],
